71d5a2f0cbc0a46100ca5cdb86e999a5481d524b
lewmilburn <lewiscmilburn@gmail.com>
2023-10-26 15:10:20 +0100

    Added token generation code

--- Web/authentication/authenticationManager.php
+++ Web/authentication/authenticationManager.php
@@ -2,11 +2,35 @@
 
 namespace Vault\Authentication;
 
+use mysql_xdevapi\Exception;
 use Vault\Data\dataManager;
 
 class authenticationManager {
     public function Login(string $username, string $password) {
-        $data = new dataManager();
-        $data->getUserData($username);
+        if (session_status() == PHP_SESSION_ACTIVE) {
+            $data = new dataManager();
+            $user = $data->getUserData($username);
+            if (password_verify($password, $user->password)) {
+                $tm = new tokenManager();
+                $token = $tm->generateToken($user->uuid);
+                $_SESSION['uuid'] = $user->uuid;
+                $_SESSION['token'] = $token;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            throw new Exception('No active session.');
+        }
+    }
+
+    public function Logout() {
+        if (session_status() == PHP_SESSION_ACTIVE) {
+            session_unset();
+            session_destroy();
+            return true;
+        } else {
+            throw new Exception('No active session.');
+        }
     }
 }
