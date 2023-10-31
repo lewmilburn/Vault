02c8490639849faec2ffaf17991aac4cd6f7b405
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-10-31 22:21:01 +0000

    Added post request & refactored to prevent duplicate code

--- Web/event/routeHandler.php
+++ Web/event/routeHandler.php
@@ -2,22 +2,39 @@
 
 namespace Vault\event;
 
+use JetBrains\PhpStorm\NoReturn;
+
 class routeHandler
 {
+    #[NoReturn]
     public function getRequest($url, $file): void
     {
-        if ($_SERVER['REQUEST_URI'] === $url && $_SERVER['REQUEST_METHOD'] === 'GET') {
-            $this->displayFile($file);
+        $this->request($url, $file, 'GET');
+    }
+
+    #[NoReturn]
+    public function postRequest(string $url, string $file): void
+    {
+        $this->request($url, $file, 'POST');
+    }
+
+    #[NoReturn]
+    public function request(string $url, string $file, string $method): void
+    {
+        if ($_SERVER['REQUEST_URI'] === $url && $_SERVER['REQUEST_METHOD'] === $method) {
+            $this->runFile($file);
         }
     }
 
+    #[NoReturn]
     public function endRouter(): void
     {
         $eh = new errorHandler();
         $eh->fileNotFound('event', 'routeHandler', 'endRouter');
     }
 
-    private function displayFile($file): void
+    #[NoReturn]
+    private function runFile($file): void
     {
         if (file_exists(__DIR__.'/../'.$file)) {
             require_once __DIR__.'/../'.$file;
