512578a94814a3c276e3e12245a9b216c545b218
lewmilburn <lewiscmilburn@gmail.com>
2023-11-01 20:22:56 +0000

    Fix bad namespace

--- Web/security/InputManager.php
+++ Web/security/InputManager.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Vault;
+namespace Vault\security;
 
 class InputManager
 {
--- Web/view/login.php
+++ Web/view/login.php
@@ -1,5 +1,5 @@
 <?php
-use Vault\InputManager;
+use Vault\security\InputManager;
 ?><!DOCTYPE html>
 <html lang="en">
     <head>
