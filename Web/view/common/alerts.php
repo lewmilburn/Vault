0b4cef70f35c844296edc0f1108fb2ef7c1b0727
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-11-13 05:52:07 +0000

    Updated CSS

--- Web/view/common/alerts.php
+++ Web/view/common/alerts.php
@@ -1,8 +1,11 @@
-<?php
-
-if (ENV == DEV) { ?>
+<?php if (ENV == DEV) { ?>
 <div class="alert-red">
     Vault is in development mode.
     You should set ENV to PROD if you're running this in a production environment.
 </div>
+<?php } ?>
+<?php if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off") { ?>
+    <div class="alert-red">
+        You are using insecure HTTP. <a href="https://github.com/lewmilburn/Vault/wiki/Security#http-vs-https">Learn more here</a>.
+    </div>
 <?php } ?>
