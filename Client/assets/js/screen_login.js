d0512c4421ac71cf0c0791b6da70469e31160d59
Lewis Milburn <lewiscmilburn@gmail.com>
2024-04-17 14:19:03 +0100

    Change: Change to alerts.

--- Client/assets/js/screen_login.js
+++ Client/assets/js/screen_login.js
@@ -14,7 +14,6 @@ function doLogin() {
         body: JSON.stringify(data),
     }).then(response => response.json())
         .then(jsonResponse => {
-            let errorBox = document.getElementById('error');
             if (jsonResponse.status === 200) {
                 if (jsonResponse.apikey !== undefined) {
                     localStorage.setItem('name', jsonResponse.name);
@@ -24,33 +23,29 @@ function doLogin() {
 
                     screenDashboard();
                 } else {
-                    errorBox.classList.remove('hidden');
-                    errorBox.innerHTML = 'Unable to fetch key, please check the "SEND_ALL_INFO" setting is enabled on the server.';
+                    alert('Unable to fetch key, please check your server is online, restart Vault, and try again.');
                 }
             } else if (jsonResponse.status === 401) {
-                errorBox.classList.remove('hidden');
-                errorBox.innerHTML = 'Username or password is incorrect.';
+                alert('Username or password is incorrect.');
             } else if (jsonResponse.error === 'Already authenticated.') {
-                errorBox.classList.remove('hidden');
-                errorBox.innerHTML = 'Already authenticated, please clear your cookies and try again.';
+                alert('Already authenticated, please clear your cookies and try again.');
             } else if (jsonResponse.error === 'Missing required data.') {
-                errorBox.classList.remove('hidden');
-                errorBox.innerHTML = 'Please enter a username and password.';
+                alert('Please enter a username and password.');
             } else if (jsonResponse.error === 'Two-factor authentication code does not match.') {
-                errorBox.classList.remove('hidden');
-                errorBox.innerHTML = 'Two-factor authentication code does not match.';
+                alert('Two-factor authentication code does not match.');
             } else if (jsonResponse.status === 500) {
-                errorBox.classList.remove('hidden');
-                errorBox.innerHTML = 'An error occurred on the server whilst trying to authenticate you, please try again.';
+                alert('An error occurred on the server whilst trying to authenticate you, please try again.');
             } else if (jsonResponse.status === 404) {
-                errorBox.classList.remove('hidden');
-                errorBox.innerHTML = 'Unable to connect to Vault, please check your settings and try again.';
+                alert('Unable to connect to Vault, please check your settings and try again.');
             } else if (jsonResponse.status === 400) {
-                errorBox.classList.remove('hidden');
-                errorBox.innerHTML = 'Required data was not recieved by the server, please try again.';
+                alert('Required data was not received by the server, please try again.');
+            } else if (jsonResponse.status === 403) {
+                alert('Two-factor authentication code does not match.');
+            } else if (jsonResponse.status === 302) {
+                alert('The server responded with error 302, this indicates that the Vault Server has been moved. ' +
+                    'Please check your installation and try again.');
             } else {
-                errorBox.classList.remove('hidden');
-                errorBox.innerHTML = 'An unexpected error occurred.';
+                alert('An unexpected error occurred.');
             }
         })
         .catch(xhr => {
